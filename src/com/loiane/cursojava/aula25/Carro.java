8179cf57fd6912b73b5579960f858ad3aa2a06e5
JulianaPetravicius <julianacelinopetra@gmail.com>
2023-08-16 16:31:10 +0100

    Método com retorno e Método com parâmetro.

---
+++ src/com/loiane/cursojava/aula25/Carro.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula25;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+    void exibirAutonomia() {
+        System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + "km");
+    }
+
+
+}
+
