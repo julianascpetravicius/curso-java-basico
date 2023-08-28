268276298da033bb0a5b0a05621d54c868d7216e
JulianaPetravicius <julianacelinopetra@gmail.com>
2023-08-28 17:21:51 +0100

    Aula 33.

---
+++ src/com/loiane/cursojava/aula33/labs/exercicio01/Exer01.java
@@ -0,0 +1,26 @@
+package com.loiane.cursojava.aula33.labs.exercicio01;
+
+import com.loiane.cursojava.aula33.labs.exercicio01.Lampada;
+
+public class Exer01 {
+
+    public static void main(String[] args){
+
+        Lampada lampada = new Lampada();
+
+        lampada.mostrarEstado();
+
+        lampada.desligar();
+
+        lampada.mostrarEstado();
+
+        lampada.mudarEstado();
+
+        lampada.mostrarEstado();
+
+    }
+
+
+}
+
+
