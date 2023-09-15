509bf88bb9af762afdddfe48e74d746bed090dcf
Juliana Petravicius <julianacelinopetra@gmail.com>
2023-09-15 20:15:18 -0300

    Ecercício 02 aula 43.

---
+++ src/com/loiane/cursojava/aula43/labs/exercicio02/Teste.java
@@ -0,0 +1,64 @@
+package com.loiane.cursojava.aula43.labs.exercicio02;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        System.out.println("*** Animais do Zoo ****");
+
+        Animal animal = new Animal();
+        animal.setNumPatas(4);
+
+
+        Peixe peixe = new Peixe();
+        peixe.setNome("Peixe");
+        peixe.setNumPatas(0);
+        peixe.setAmbiente("mar");
+        peixe.setCor("cinzenta");
+        peixe.setCaracteristicas("barbatana e cauda");
+
+        Peixe tubarao = new Peixe();
+        tubarao.setNome("Tubarao");
+        tubarao.setComprimento(300);
+        tubarao.setVelocidade(1.5);
+        tubarao.setAmbiente("mar");
+        tubarao.setCor("Cinzento");
+        tubarao.setCaracteristicas("barbatana e cauda");
+
+        tubarao.imprimirAnimais();
+        System.out.println("-----------------");
+
+        Mamifero mamifero =new Mamifero();
+        mamifero.setAmbiente("terra");
+
+        Mamifero camelo = new Mamifero();
+        camelo.setNome("camelo");
+        camelo.setComprimento(150);
+        camelo.setCor("amarelo");
+        camelo.setVelocidade(2.0);
+        camelo.setAmbiente("terra");
+        camelo.setNumPatas(4);
+
+        camelo.imprimirAnimais();
+        System.out.println("-----------------");
+
+        Urso urso = new Urso();
+        urso.setCor("castanho");
+        urso.setAlimento("mel");
+
+
+        Urso ursoDoCanada =new Urso();
+        ursoDoCanada.setNome("Urso do Canadá");
+        ursoDoCanada.setComprimento(180);
+        ursoDoCanada.setCor("vermelho");
+        ursoDoCanada.setVelocidade(0.5);
+        ursoDoCanada.setNumPatas(4);
+        ursoDoCanada.setAmbiente("terra");
+        ursoDoCanada.setAlimento("mel");
+
+
+        ursoDoCanada.imprimirAnimais();
+        System.out.println("-----------------");
+
+
+    }
+}
---
+++ src/com/loiane/cursojava/aula43/labs/exercicio02/Urso.java
@@ -0,0 +1,27 @@
+package com.loiane.cursojava.aula43.labs.exercicio02;
+
+public class Urso extends Mamifero {
+    public Urso() {
+    }
+
+    public Urso(String nome, double comprimento, int numPatas, String cor, String ambiente, double velocidade) {
+        super(nome, comprimento, numPatas, cor, ambiente, velocidade);
+    }
+
+    @Override
+    public String toString() {
+        return "Urso{} " + super.toString();
+    }
+    public void imprimirAnimais(){
+
+
+        System.out.println("nome: " + getNome());
+        System.out.println("comprimento: " + getComprimento());
+        System.out.println("Numero de Patas: " + getNumPatas(0));
+        System.out.println("Cor: " + getCor("cinzento"));
+        System.out.println("Ambiente: " + getAmbiente("terra"));
+        System.out.println("Velocidade: " + getVelocidade() + " m/s ");
+        System.out.println("Alimento: " + getAlimento());
+
+    }
+}
